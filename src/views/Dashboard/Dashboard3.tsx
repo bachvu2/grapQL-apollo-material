b51f37a81d3781e46e08e3e16cdcdc1795d43580
bachvu2 <85206533+bachvu2@users.noreply.github.com>
2021-06-02 11:12:58 +0700

    first commit

---
+++ src/views/Dashboard/Dashboard3.tsx
@@ -0,0 +1,11 @@
+import React, { memo, useCallback, useEffect } from 'react';
+
+export const Dashboard3: React.FC = () => {
+    return (
+        <div>
+            <h3>heelo</h3>
+        </div>
+    )
+}
+export default Dashboard3;
+
